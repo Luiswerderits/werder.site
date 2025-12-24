8e1ff0eef23c70055517b5c5dd7cb9546e3009c3
Luiswerderits <luiswerderits@gmail.com>
2025-12-24 01:18:32 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// LOADER
+window.addEventListener("load", () => {
+  document.getElementById("loader").style.display = "none";
+});
+
+// ANIMAÇÃO DE ENTRADA
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach(sec => {
+  sec.style.opacity = 0;
+  sec.style.transform = "translateY(40px)";
+  sec.style.transition = "0.8s";
+  observer.observe(sec);
+});
